2f2ec08db7540c84b2d52e62bd3d3197ae0ae59a
Ran Pan <panpanr@gmail.com>
2025-12-12 08:12:16 -0500

    Add Google AdSense

---
+++ PdfEdit.ps1
@@ -0,0 +1,12 @@
+﻿git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/panpanran/PdfEdit.git
+git push -u origin main
+
+git add .
+git commit -m "Add Google AdSense"
+git push
+
+cd C:\Users\panpa\Notes\Project\PdfEdit
